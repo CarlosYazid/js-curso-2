fb6580a806ff74a8bc2a1270a506a8f60f0c9d35
Carlos Yazid <140143753+CarlosYazid@users.noreply.github.com>
2024-01-18 17:49:58 -0500

    MY_task
    Hice las tareas opcionales

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 
+document.querySelector('h1').innerHTML = "Hora del desafio";
+
+
+
+function click_() {
+    console.log("El boton fue clickeado");
+};
+
+function city() {
+    city_ = prompt("Ingrese su ciudad:");
+    alert(`Estuve en ${city_} y me acorder de vos`);
+};
+
+function alert_js() {
+    alert("Yo amo js");
+}
+
+function sum_() {
+    num1 = parseInt(prompt("Ingrese el primer numero:"));
+    num2 = parseInt(prompt("Ingrese el segundo numero:"));
+    alert(`La suma de los numeros es: ${num1 + num2}`);
+}
